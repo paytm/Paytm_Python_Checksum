b468008178e9846043ab6db8edbab46d065e649d
Kaji Nasiruddin Ahmed <nasir.ciem.it@gmail.com>
2022-10-25 14:52:25 +0530

    clean sample

--- sample.py
+++ sample.py
@@ -1,9 +1,6 @@
 # More Details: https://developer.paytm.com/docs/checksum/#python
 
-import requests
-import json
-
-from paytmchecksum.checksum import PaytmCheckSum
+from paytmchecksum import PaytmCheckSum
 
 # Generate Checksum via Hash/Array
 # initialize an Hash/Array
@@ -24,7 +21,7 @@ print("verifySignature Returns:" + str(verifyChecksum))
 
 # Generate Checksum via String
 # initialize JSON String
-body = json.dumps({"mid": "YOUR_MID_HERE", "orderId": "YOUR_ORDER_ID_HERE"})
+body = '{"mid": "YOUR_MID_HERE", "orderId": "YOUR_ORDER_ID_HERE"}'
 
 # Generate checksum by parameters we have
 # Find your Merchant Key in your Paytm Dashboard at https://dashboard.paytm.com/next/apikeys
